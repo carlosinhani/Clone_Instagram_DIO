234675286c7aca356b38944b8654d09ef1b4c80b
Carlos.Inhani <carlos.inhani@gmail.com>
2020-11-10 19:58:09 -0300

    animated

--- src/Pages/Feed/index.js
+++ src/Pages/Feed/index.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState, useEffect } from 'react';
+import { FlatList } from "react-native";
+import api from '../../services/api';
+import FeedImage from '../../components/FeedImage';
+import {
+  Container,
+  Post,
+  Header,
+  Avatar,
+  Name,
+  Description,
+  Loading
+} from './styles';
 
 
-const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     justifyContent: 'center',
-     alignItems: "center",
-     backgroundColor: '#F5F5F5',
-   },
-   title: {
-     fontSize: 24,
-     textAlign: 'center',
-     margin: 10,
-   },
-});
+export default function Feed() {
+  const [feed, setFeed] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [refreshing, setRefreshing] = useState(false)
 
-export default function App() {
+  async function loadPage() {
+    setLoading(true)
+
+    const { data } = await api.get('/feed?_expand=author')
+
+    setLoading(false)
+    setFeed(data)
+  }
+
+  useEffect(() => {
+    loadPage();
+  }, [])
+
+  async function refreshList() {
+    setRefreshing(true)
+
+    await loadPage();
+
+    setRefreshing(false)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello Agora Vai</Text>
-    </View>
+    <Container>
+      <FlatList
+      key="list"
+      data={feed}
+      keyExtractor={(item) => String(item.id)}
+      viewabilityConfig={{
+        viewAreaCoveragePercentThreshold: 10,
+      }}
+      showsVerticalScrollIndicator={false}
+      onRefresh={refreshList}
+      refreshing={refreshing}
+      ListFooterComponent={loading && <Loading />}
+      renderItem={({ item }) => (
+        <Post>
+          <Header>
+            <Avatar source={{ uri: item.author.avatar }} />
+            <Name>{item.author.name}</Name>
+          </Header>
+          <FeedImage
+            aspectRatio={item.aspectRatio}
+            smallSource={{ uri: item.image }}
+            source={{ uri: item.small}} />
+          <Description>
+            <Name>{item.author.name}</Name> {item.description}
+          </Description>
+        </Post>
+      )}
+      />
+    </Container>
   );
 }
